1b20ed173692aefa7b210f4d43653641bea43d78
Kinhs <bangtanboy7b@gmail.com>
2025-05-01 14:31:19 +0700

    Added Splash screen

---
+++ app/src/main/java/com/group8/bloodbank/activities/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.group8.bloodbank.activities;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.group8.bloodbank.MainActivity;
+import com.group8.bloodbank.R;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
